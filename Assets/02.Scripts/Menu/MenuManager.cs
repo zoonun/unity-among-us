9403b323ac3eeb128608f7c7cecb9a0cbfdc5ff4
engcraft1102 <engcraft1102@gmail.com>
2022-08-23 11:54:57 +0900

    Start, End game

--- Assets/02.Scripts/Menu/MenuManager.cs
+++ Assets/02.Scripts/Menu/MenuManager.cs
@@ -10,6 +10,7 @@ public class MenuManager : MonoBehaviour
 {
     private UIDocument _doc;
     private VisualElement _root;
+    private VisualElement _rootContainer;
 
     // Setting
     private VisualElement _settingContainer;
@@ -17,6 +18,10 @@ public class MenuManager : MonoBehaviour
     private Button _settingCloseBtn;
     private Button _settingMouseBtn;
     private Button _settingKeyboardMouseBtn;
+    
+    // Game Control
+    private Button _onlineBtn;
+    private Button _endBtn;
     
     
     // Start is called before the first frame update
@@ -26,20 +31,34 @@ public class MenuManager : MonoBehaviour
         {
             _doc = GetComponent<UIDocument>();
             _root = _doc.rootVisualElement;
+            _rootContainer = _root.Q<VisualElement>("root_container");
             
             // Settings UI
             _settingContainer = _root.Q<VisualElement>("setting_container");
             _settingContainer.style.scale = new StyleScale(new Scale(new Vector2(0, 0)));
             _settingBtn = _root.Q<Button>("btn__setting");
-            _settingBtn.clicked += SettingClick;
+            _settingBtn.clicked += OnClickSetting;
             _settingCloseBtn = _root.Q<Button>("btn__setting__close");
-            _settingCloseBtn.clicked += SettingCloseClick;
+            _settingCloseBtn.clicked += OnClickSettingClose;
             
+            // 세팅 창 외의 영역을 클릭하면 닫히도록 설정
+            // _rootContainer.RegisterCallback<ClickEvent>(evt =>
+            // {
+            //     Debug.Log(evt.currentTarget.ToString());
+            //     OnClickSettingClose();
+            // });
+
             // Settings Control
             _settingMouseBtn = _root.Q<Button>("btn__setting__mouse");
-            _settingMouseBtn.clicked += SettingMouseClick;
+            _settingMouseBtn.clicked += OnclickMouseSetting;
             _settingKeyboardMouseBtn = _root.Q<Button>("btn__setting__keyboard__mouse");
-            _settingKeyboardMouseBtn.clicked += SettingKeyboardMouseClick;
+            _settingKeyboardMouseBtn.clicked += OnClickKeyboardMouseSetting;
+            
+            // Game Control
+            _onlineBtn = _root.Q<Button>("btn__online");
+            _onlineBtn.clicked += OnClickOnline;
+            _endBtn = _root.Q<Button>("btn__end");
+            _endBtn.clicked += OnClickEnd;
             
             switch (PlayerSetting.controlType)
             {
@@ -60,20 +79,20 @@ public class MenuManager : MonoBehaviour
         }
     }
 
-    private void SettingClick()
+    private void OnClickSetting()
     {
+        _settingContainer.style.opacity = 1;
         _settingContainer.style.scale = new StyleScale(new Scale(new Vector2(1, 1)));
-    } 
-    
-    private void SettingCloseClick()
+    }
+    private void OnClickSettingClose()
     {
         _settingContainer.style.scale = new StyleScale(new Scale(new Vector2(0, 0)));
     }
-    private void SettingMouseClick()
+    private void OnclickMouseSetting()
     {   
         SetControlMode(0);
     }
-    private void SettingKeyboardMouseClick()
+    private void OnClickKeyboardMouseSetting()
     {
         SetControlMode(1);
     }
@@ -94,5 +113,18 @@ public class MenuManager : MonoBehaviour
                 _settingKeyboardMouseBtn.style.color = Color.green;
                 break;
         }
+    }
+
+    public void OnClickOnline()
+    {
+        Debug.Log("Click Online");
+    }
+    public void OnClickEnd()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();        
+#endif
     }
 }
